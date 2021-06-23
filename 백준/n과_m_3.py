3cb1844457a24f24e124f43b7f5bec56f2fa9595
unknown <hh4518@gmail.com>
2021-06-23 11:56:12 +0900

    백트래킹 연습

---
+++ 백준/n과_m_3.py
@@ -0,0 +1,15 @@
+from sys import stdin
+input = stdin.readline
+
+n, m = map(int, input().split())
+
+def recur(result):
+    if len(result) == m:
+        print(*result)
+        return
+    for i in range(1, n + 1):
+        result.append(i)
+        recur(result)
+        result.pop()
+
+recur(list())
