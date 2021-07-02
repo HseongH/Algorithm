430cf40541c46402f9f175deff52e45f4006a2df
unknown <hh4518@gmail.com>
2021-07-03 00:09:07 +0900

    124 나라의 숫자 문제 수정

--- 프로그래머스/124나라.js
+++ 프로그래머스/124나라.js
@@ -1,15 +1,18 @@
 function solution(n) {
-  const arr = ['1', '2', '4'];
-  const number = n.toString(3);
+  let answer = '';
+  const numbers = [4, 1, 2];
+
+  while (n) {
+    const index = n % 3;
+
+    n = Math.floor(n / 3);
+
+    if (!index) n--;
+
+    answer = numbers[index] + answer;
+  }
+
+  return answer;
 }
 
-console.log(solution(1));
-console.log(solution(2));
-console.log(solution(3));
-console.log(solution(4));
-console.log(solution(5));
-console.log(solution(6));
-console.log(solution(7));
-console.log(solution(8));
-console.log(solution(9));
-console.log(solution(10));
+console.log(solution(5555555));
