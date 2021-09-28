fd814f3bc2d41a9908e55f6d12d63b63fca64360
unknown <hh4518@gmail.com>
2021-09-28 23:57:38 +0900

    백준 알고리즘 문제 풀이

--- 백준/단어뒤집기2.js
+++ 백준/단어뒤집기2.js
@@ -1,6 +1,33 @@
 const input = require('fs').readFileSync('예제.txt').toString();
 const result = [];
 
-const regExp = /<+\w>+/g;
-const string = input.match(regExp);
-// console.log(result.join(' '));
+let convert = true;
+let target = [];
+
+for (let i = 0; i < input.length; i++) {
+  const word = input[i];
+
+  if (word === '<') {
+    if (target.length) {
+      result.push(target.join(''));
+      target.splice(0, target.length);
+    }
+
+    convert = false;
+  } else if (word === '>' || (word === ' ' && convert)) {
+    target.push(word);
+    result.push(target.join(''));
+    target.splice(0, target.length);
+
+    continue;
+  }
+
+  convert ? target.unshift(word) : target.push(word);
+  console.log(target);
+}
+
+if (target.length) {
+  result.push(target.join(''));
+}
+
+console.log(result.join(''));
