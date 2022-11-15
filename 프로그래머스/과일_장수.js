54e74bc6384e3cec9fb26055c8b3464e3997a548
HseongH <hh4518@gmail.com>
2022-11-15 21:47:05 +0900

    과일장수 문제 불필요 로직 제거

--- 프로그래머스/과일_장수.js
+++ 프로그래머스/과일_장수.js
@@ -1,9 +1,8 @@
 function solution(k, m, score) {
-  const count = Math.floor(score.length / m);
-  const target = score.sort((a, b) => b - a).slice(0, count * m);
+  score.sort((a, b) => b - a);
 
-  return Array.from({ length: count }).reduce(
-    (acc, _, index) => (acc += target[m - 1 + m * index] * m),
+  return Array.from({ length: Math.floor(score.length / m) }).reduce(
+    (acc, _, index) => (acc += score[m - 1 + m * index] * m),
     0
   );
 }
---
+++ 프로그래머스/삼총사.js
@@ -0,0 +1,6 @@
+function solution(number) {
+  const answer = 0;
+  return answer;
+}
+
+console.log(solution([-2, 3, 0, 2, -5]));
